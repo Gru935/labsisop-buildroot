279b0d8a2a80e86fcf1442cfde87f944fde23851
Gru935 <133711873+Gru935@users.noreply.github.com>
2025-05-16 21:55:53 +0000

    Parei no tutorial 3.1 - 3.3.2

--- linux-4.13.9/kernel/sched/core.c
+++ linux-4.13.9/kernel/sched/core.c
@@ -747,6 +747,13 @@ static void set_load_weight(struct task_struct *p)
 		return;
 	}
 
+	// SCHED_LOW_IDLE
+	if (idle_low_policy(p->policy)) {
+		load->weight = scale_load(WEIGHT_LOW_IDLEPRIO);
+		load->inv_weight = WMULT_LOW_IDLEPRIO;
+		return;
+	}
+
 	load->weight = scale_load(sched_prio_to_weight[prio]);
 	load->inv_weight = sched_prio_to_wmult[prio];
 }
