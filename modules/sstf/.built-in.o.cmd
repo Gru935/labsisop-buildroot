8b0894d5c225d678fb72cb6739eac43d8cc0fba9
Gru935 <133711873+Gru935@users.noreply.github.com>
2025-06-08 21:23:54 +0000

    Fds sla n aguento mais essa bosta aki tu paga pelo negocio e n tem acesso pqp, fui pro tutorial 3.2

--- modules/sstf/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/workspaces/labsisop-buildroot/modules/sstf/built-in.o :=  rm -f /workspaces/labsisop-buildroot/modules/sstf/built-in.o; ar rcSTPD /workspaces/labsisop-buildroot/modules/sstf/built-in.o
